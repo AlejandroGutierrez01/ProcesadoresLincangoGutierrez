443ba3317635631b06b021762308c5cc35aba7de
JoseEduardo23 <jose.lincango@epn.edu.ec>
2024-05-30 08:19:38 -0500

    Implementacion de la clase Procesadores_moviles MS

--- src/Procesadores/Procesadores_AMD.java
+++ src/Procesadores/Procesadores_AMD.java
@@ -1,13 +1,12 @@
-
 package Procesadores;
 
-public class Procesadores_AMD extends Procesadores {
+public class Procesadores_AMD extends Procesadores_escritorio {
     boolean sensero_temperatura;
 
     public Procesadores_AMD() {}
 
-    public Procesadores_AMD(int nucleos, String nombre, double velocidad, double consumo, boolean sensero_temperatura) {
-        super(nucleos, nombre, velocidad, consumo);
+    public Procesadores_AMD(int nucleos, String nombre, double velocidad, double consumo, String socket, boolean sensero_temperatura) {
+        super(nucleos, nombre, velocidad, consumo, socket);
         this.sensero_temperatura = sensero_temperatura;
     }
 
@@ -18,8 +17,7 @@ public class Procesadores_AMD extends Procesadores {
     public void setSensero_temperatura(boolean sensero_temperatura) {
         this.sensero_temperatura = sensero_temperatura;
     }
-    public void imprimir_datos_escritorio()
-    {
+    public void imprimir_datos_Procesadores_AMD(){
         System.out.println("Sensor de temperatura: " + this.sensero_temperatura);
     }
 }
