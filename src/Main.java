d4ee785836441f49db3976b382277eca9095f9e4
alejandro4-admin <alejandro.gutierrez01@epn.edu.ec>
2024-05-30 09:08:49 -0500

    Ingreso de los objetos con y sin atributos de los procesadores Intel

--- src/Main.java
+++ src/Main.java
@@ -9,12 +9,12 @@ public class Main {
         //integrante Michelle Suarez
         Procesadores_moviles prom1 = new Procesadores_moviles();
 
-        Procesadores_moviles prom2 = new Procesadores_moviles(4, "Snapdragon de Qualcomm",5, 2.4, true );
+        Procesadores_moviles prom2 = new Procesadores_moviles(4, "Snapdragon de Qualcomm", 5, 2.4, true);
         //Integrante Alejandro Gutiérrez
 
         Procesadores_INTEL proIn1 = new Procesadores_INTEL();
 
-        Procesadores_INTEL proIn2 = new Procesadores_INTEL(4,"Intel Core i5",2.4,3.5,"LGA",false);
+        Procesadores_INTEL proIn2 = new Procesadores_INTEL(4, "Intel Core i5", 2.4, 3.5, "LGA", false);
 
     }
 }
