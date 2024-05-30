d2ac4dac12d3e83ce79dd8e2c06f3ecd54a086fd
JoseEduardo23 <jose.lincango@epn.edu.ec>
2024-05-30 08:50:30 -0500

    .

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,7 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 import Procesadores.Procesadores;
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         Procesadores p1 = new Procesadores();
     }
 }
