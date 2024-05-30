88c0c62e242e8fcc45d1f080d639d77a870a63a0
michelle2444444 <almeandrapark@gmail.com>
2024-05-30 08:45:26 -0500

    Ingreso de instancia vacia procesador movil MS

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,15 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 import Procesadores.Procesadores;
+import Procesadores.Procesadores_AMD;
+import Procesadores.Procesadores_moviles;
+import Procesadores.Procesadores_escritorio;
+import Procesadores.Procesadores_INTEL;
 public class Main {
     public static void main(String[] args) {
-        Procesadores p1 = new Procesadores();
+
+        //integrante Michelle Suarez
+        Procesadores_moviles prom1 = new Procesadores_moviles();
+
+        Procesadores_moviles prom2 = new Procesadores_moviles(4, "Snapdragon de Qualcomm",5, 2.4, true );
+
     }
 }
